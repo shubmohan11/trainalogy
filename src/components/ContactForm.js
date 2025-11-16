3e4feb279ea792f4cde008eefcef915df514de77
Shubham Mohan <shubhammohan@Shubhams-MacBook-Pro.local>
2025-11-16 17:51:31 +0530

    ContactForm: copy/layout refinements and polishing; sync with latest design

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -43,7 +43,7 @@ export default function ContactForm() {
             </div>
           </div>
           
-          <button type="submit" className="form-submit">yfitnessdxb.com</button>
+          <button type="submit" className="form-submit">Submit</button>
         </form>
       </div>
     </section>
